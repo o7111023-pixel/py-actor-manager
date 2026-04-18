1e49c96bc9f73b81a1ce6bc371bba3ecdbe09f4c
Oleh M <oleh.murachov28@gmail.com>
2026-04-18 20:23:36 +0200

    okk

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,49 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name, table_name):
+        self.db_name = db_name
+        self.table_name = table_name
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+
+    def create(self, first_name, last_name):
+        query = f"""
+        INSERT INTO {self.table_name} (first_name, last_name)
+        VALUES (?, ?)
+        """
+        self.cursor.execute(query, (first_name, last_name))
+        self.connection.commit()
+
+
+    def all(self):
+        query = f"SELECT id, first_name, last_name FROM {self.table_name}"
+        self.cursor.execute(query)
+
+        rows = self.cursor.fetchall()
+
+        return [
+            Actor(id=row[0], first_name=row[1], last_name=row[2])
+            for row in rows
+        ]  # если нет записей → []
+
+
+    def update(self, pk, new_first_name, new_last_name):
+        query = f"""
+        UPDATE {self.table_name}
+        SET first_name = ?, last_name = ?
+        WHERE id = ?
+        """
+        self.cursor.execute(query, (new_first_name, new_last_name, pk))
+        self.connection.commit()
+
+
+    def delete(self, pk):
+        query = f"DELETE FROM {self.table_name} WHERE id = ?"
+        self.cursor.execute(query, (pk,))
+        self.connection.commit()
+
+
+    def close(self):
+        self.connection.close()
--- py-actor-manager/app/models.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Actor:
-        id: int
-        first_name: str
-        last_name: str
