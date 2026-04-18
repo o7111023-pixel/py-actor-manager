9c11e62d1854381cd7d7cd0ab5b355d6940f407e
Oleh M <oleh.murachov28@gmail.com>
2026-04-18 20:27:22 +0200

    opp

--- app/managers.py
+++ app/managers.py
@@ -1,16 +1,17 @@
 import sqlite3
+from typing import List
 
 from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name, table_name):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         query = f"""
         INSERT INTO {self.table_name} (first_name, last_name)
         VALUES (?, ?)
@@ -18,20 +19,17 @@ class ActorManager:
         self.cursor.execute(query, (first_name, last_name))
         self.connection.commit()
 
-
-    def all(self):
+    def all(self) -> List[Actor]:
         query = f"SELECT id, first_name, last_name FROM {self.table_name}"
         self.cursor.execute(query)
-
         rows = self.cursor.fetchall()
 
         return [
             Actor(id=row[0], first_name=row[1], last_name=row[2])
             for row in rows
-        ]  # если нет записей → []
+        ]
 
-
-    def update(self, pk, new_first_name, new_last_name):
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         query = f"""
         UPDATE {self.table_name}
         SET first_name = ?, last_name = ?
@@ -40,12 +38,10 @@ class ActorManager:
         self.cursor.execute(query, (new_first_name, new_last_name, pk))
         self.connection.commit()
 
-
-    def delete(self, pk):
+    def delete(self, pk: int) -> None:
         query = f"DELETE FROM {self.table_name} WHERE id = ?"
         self.cursor.execute(query, (pk,))
         self.connection.commit()
 
-
-    def close(self):
+    def close(self) -> None:
         self.connection.close()
